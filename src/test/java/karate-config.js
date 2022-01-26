9a8ccf6cbc24232085236f81630142d42334588f
Deepa Perera <deepa.perera@acc.co.nz>
2022-01-27 11:25:54 +1300

    initial commit

---
+++ src/test/java/karate-config.js
@@ -0,0 +1,19 @@
+function fn() {
+  var env = karate.env; // get system property 'karate.env'
+  karate.log('karate.env system property was:', env);
+  if (!env) {
+    env = 'dev';
+  }
+  var config = {
+    env: env,
+    myVarName: 'someValue'
+  }
+  if (env == 'dev') {
+    // customize
+    // e.g. config.foo = 'bar';
+  } else if (env == 'e2e') {
+    // customize
+  }
+   karate.configure('proxy', 'http://webproxy.ds.acc.co.nz:8080');
+  return config;
+}
